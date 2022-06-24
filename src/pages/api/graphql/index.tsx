8777b173e757f0c4312908e8bf48efe01996644e
Yagiz Nizipli <yagiz@nizipli.com>
2022-06-24 09:52:22 -0400

    fix: update cors settings for graphql

--- src/pages/api/graphql/index.tsx
+++ src/pages/api/graphql/index.tsx
@@ -9,7 +9,7 @@ const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   context,
-  introspection: true,
+  introspection: false,
 })
 
 const startServer = apolloServer.start()
@@ -17,6 +17,20 @@ const startServer = apolloServer.start()
 export const config = { api: { bodyParser: false } }
 
 const handler: NextApiHandler = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
   await startServer
   await apolloServer.createHandler({ path: '/api/graphql' })(req, res)
 }
